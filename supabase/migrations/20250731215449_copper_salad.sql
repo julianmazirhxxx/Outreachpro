c5499817a8321437406414efb1452563f96e89ac
julianmazirhxxx <imadiomazirio@gmail.com>
2025-07-31 23:54:55 +0200

    Fix cleanup to preserve lead_sequence_progress for live test

---
+++ supabase/migrations/20250731215449_copper_salad.sql
@@ -0,0 +1,5 @@
+@@ .. @@
+ -- Remove all lead sequence progress except for the live test campaign
+ DELETE FROM lead_sequence_progress 
+-WHERE campaign_id != 'be239e0b-d75e-4ffc-ba0c-b693d157cb89';
++WHERE campaign_id != 'be239e0b-d75e-4ffc-ba0c-b693d157cb89';
